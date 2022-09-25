b9eed3e5cc0a2594dbf622c25d76e466360e4353
Deepa Nair <104196538+Deepsgk@users.noreply.github.com>
2022-09-26 03:19:02 +1000

    changes

--- project3nobel/app.py
+++ project3nobel/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, render_template, request, url_for, redirect
 from requests import session
 import psycopg2
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,SQLAlchemy
 from config import pg_pass
 from config import pg_user
 import psycopg2
@@ -12,58 +12,109 @@ from flask import Response
 import json
 import plotly.express as px
 import plotly
-
-# create engine for postgresql
-rds_connection_string = (f'{pg_user}:{pg_pass}@localhost:5432/Nobel_prize_db')
-engine = create_engine(f'postgresql://{rds_connection_string}')
-
-app= Flask(__name__)
+import os
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+#################################################
+# Database Setup
+#################################################
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or 'postgresql://localhost/[Nobel_prize_db]'
+
+# Remove tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# flask setup
 
 
-def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='Nobel_prize_db',
-                            user='postgres',
-                            password='4167')
-    return conn
-
- 
 @app.route("/")
-@cross_origin()
-def homepage():
-    return (
-        f"Welcome to NOBELPRIZE API <br/>"
-        f"Available Routes:<br/>"
-        f"/api/v0/nobelprizewinners<br/>"
-        f"/api.nobelprize.org/v1<br/>"
-        f"/api/v0/country<br/>"
-    
+def home():
+    return render_template("index.html")  
 
-    )
-
+from .models import ( nobel1_prize,
+                     country )
 
 @app.route("/api/v0/nobelprizewinners")
-def nobelprizewinners():
-    conn = get_db_connection()
-    nobelprizewinners = pd.read_sql("SELECT * FROM public.nobel1_prize", conn)
-    print(type(nobelprizewinners))
-    return Response (nobelprizewinners.to_json(orient="records"), mimetype='application/json')
+def Nobelprize():
+    results = db.session.query(nobel1_prize.awardyear, nobel1_prize.category, nobel1_prize.categortfullname,
+                               nobel1_prize.sortorder, nobel1_prize.prizeamount, nobel1_prize.motivation, 
+                               nobel1_prize.award_link, nobel1_prize.id, nobel1_prize.name, nobel1_prize.fullname, nobel1_prize.gender,
+                               nobel1_prize.laureate_link, nobel1_prize.birth_date, nobel1_prize.birth_citynow, nobel1_prize.continent,
+                               nobel1_prize.countrynow, nobel1_prize.birth_locationstring).all()
+                               
+    list= []
+    dict ={}
+   
+    for i in range(len(results)):
+             
+        dict = {
+                "awardyear"        : results[i][0],
+                "category"         : results[i][1],
+                "categoryfullname" : results[i][2],
+                "sortorder"        : results[i][3],
+                "prizeamount"      : results[i][4],
+                "motivation"       : results[i][5],
+                "award_link"       : results[i][6],
+                "id"               : results[i][7],
+                "name"             : results[i][8],
+                "fullname"         : results[i][9],
+                "gender"           : results[i][10],
+                "laureate_link"    : results[i][11],
+                "birth_date"       : results[i][12],
+                "birth_citynow"    : results[i][13],
+                "continent"        : results[i][14],
+                "countrynow"       : results[i][15],
+                "birth_locationstring" : results[i][16]
+            }
 
+        list.append(dict)
+
+    return jsonify(list)  
 @app.route("/api/v0/country")
-def country():
-    conn = get_db_connection()
-    country = pd.read_sql("SELECT * FROM public.country", conn)
-    print(type(country))
-    
-    return Response(country.to_json(orient="records"), mimetype='application/json')
+def Country():
+    results = db.session.query(country.id, country.firstname, country.surname, country.borncountry, country.borncountrycode,
+                               country.borncity, country.gender, country.year, country.category, country.motivation,
+                               country.organization_name, country.organization_city, country.organization_country, country.lattitude, country.longitude).all()
+                               
+    list= []
+    dict ={}
+   
+    for i in range(len(results)):
+             
+        dict = {
+                "id"              : results[i][0],
+                "firstname"       : results[i][1],
+                "surname"         : results[i][2],
+                "borncountry"     : results[i][3],
+                "borncountrycode" : results[i][4],
+                "borncity"        : results[i][5],
+                "gender"          : results[i][6],
+                "year"            : results[i][7],
+                "category"        : results[i][8],
+                "motivation"      : results[i][9],
+                "organization_name" : results[i][10],
+                "organization_city" : results[i][11],
+                "organization_country": results[i][12],
+                "latitude"  : results[i][13],
+                "longitude"  : results[i][14],
+                
+            }
+
+        list.append(dict)
+
+    return jsonify(list)  
 
 
 
-    
+if __name__ == "__main__":
+    app.run()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
 
 import numpy as py
 import pandas as pd
---
+++ project3nobel/models.py
@@ -0,0 +1,54 @@
+from .app import db
+
+
+class nobel1_prize(db.Model):
+    __tablename__ = 'nobel1_prize'
+    
+    awardyear        = db.Column(db.Integer) 
+    category         = db.Column(db.String(100))
+    categoryfullname = db.Column(db.String(200))
+    sortorder        = db.Column(db.Integer) 
+    prizeamount      = db.Column(db.Integer) 
+    motivation       = db.Column(db.String(1000))
+    award_link       = db.Column(db.String(100))
+    id               = db.Column(db.Integer, primary_key=True)
+    name             = db.Column(db.String(50))
+    fullname         = db.Column(db.String(100))
+    gender           = db.Column(db.String(20))
+    laureate_link    = db.Column(db.String(100))
+    birth_date       = db.Column(db.String(100))
+    birth_citynow    = db.Column(db.String(200))
+    continent        = db.Column(db.String(20))
+    countrynow       = db.Column(db.String(20))
+    birth_locationstring = db.Column(db.String(100)) 
+            
+
+    def __repr__(self):
+        return '<nobel1_prize %r>' % (self.name)
+
+
+
+class country(db.Model):
+    __tablename__ = 'country'
+
+     
+    id               = db.Column(db.Integer, primary_key=True) 
+    firstname        = db.Column(db.String(100))
+    surname          = db.Column(db.String(200))
+    borncountry      = db.Column(db.String(100)) 
+    borncountrycode  = db.Column(db.String(100)) 
+    borncity         = db.Column(db.String(1000))
+    gender           = db.Column(db.String(100))
+    year             = db.Column(db.Integer)
+    category         = db.Column(db.String(50))
+    motivation       = db.Column(db.String(100))
+    organization_name  = db.Column(db.String(20))
+    organization_city  = db.Column(db.String(100))
+    organization_country= db.Column(db.String(100))
+    country_latitude   = db.Column(db.Float)
+    country_longitude   = db.Column(db.Float)
+    
+
+    
+    def __repr__(self):
+        return '<country %r>' % (self.name)
