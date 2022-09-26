fa0e76f4754a92f6744f810d3818efa78f43d281
Deepa Nair <104196538+Deepsgk@users.noreply.github.com>
2022-09-26 15:09:43 +1000

    plotly error to be fixed

--- project3nobel/app.py
+++ project3nobel/app.py
@@ -118,34 +118,3 @@ if __name__ == "__main__":
 
 
 
-import numpy as py
-import pandas as pd
-import datetime as dt
-import matplotlib.pyplot as plt
-
-df= pd.read_json('/api/v0/nobelprizewinners')
-yearly_prize = df.groupby("awardyear")['awardyear'].count().reset_index(name = 'Count')
-fig = px.bar(yearly_prize, x = 'awardyear', y = 'Count')
-
- 
-
-category = df.groupby('category')['category'].count().reset_index(name='Count')
-fig2 = px.bar(category, x ='category', y ='Count', color='category')
-
-
-fig3 = px.histogram(df, x='prizeamount')
-
-
-fig4 = px.scatter(df, x="prizeamount", y="awardyear", color="category",
-           marginal_x="box", template="simple_white")
-
-
-fig5= px.violin(df,y= "awardyear", x="gender",color="category", box=True)
-
-with open('p_graph.html', 'a') as f:
-    f.write(fig.to_html(full_html=False, include_plotlyjs='cdn'))
-    f.write(fig2.to_html(full_html=False, include_plotlyjs='cdn'))
-    f.write(fig3.to_html(full_html=False, include_plotlyjs='cdn'))
-    f.write(fig4.to_html(full_html=False, include_plotlyjs='cdn'))
-    f.write(fig5.to_html(full_html=False, include_plotlyjs='cdn'))
-
